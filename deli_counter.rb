3025416cea627faddcb71a1695ace3ee55f56df9
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-21 01:12:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     output = "The line is currently:"
-    katz_deli.each_with_index(1) do |i|
+    katz_deli.each_with_index(1) do |person, i|
       output << " #{i}. #{katz_deli[i]}"
     end
     puts output
