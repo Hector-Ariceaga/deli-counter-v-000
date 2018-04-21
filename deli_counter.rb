9da6a6fd0315a701ed4fb6f810a74e60223ad013
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-21 01:08:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     output = "The line is currently:"
-    katz_deli.each
+    katz_deli.each_with_index
      output << " #{counter+1}. #{katz_deli[counter]}"
       counter += 1
     end
